020e2392c5f9eeb0665e192e3f9ce959a027f6af
Victor Samun <v.samun@examus.net>
2018-04-11 18:39:33 +0500

    fix tests

--- parser_test.py
+++ parser_test.py
@@ -50,15 +50,15 @@ AB,2,aa,10:00,"scorer_id: a14",14,14
 
 
 class LogsTest(unittest.TestCase):
-    DATA = r"""1:{"event_type": "edx.ui.lms.outline.selected", "event": "{\"target_url\": \"https://pages.local/1/\", \"target_name\": \"m1\"}"}
-2:{"event_type": "load_video", "event": "{\"id\": \"v1\"}", "page": "https://pages.local/1/?opt"}
+    DATA = r"""1:{"event_type": "edx.ui.lms.outline.selected", "event": "{\"target_url\": \"https://pages.local/1/0/\", \"target_name\": \"m1\"}"}
+2:{"event_type": "load_video", "event": "{\"id\": \"v1\"}", "page": "https://pages.local/1/0/?opt"}
 3:{"event_type": "play_video", "event": "{\"id\": \"v1\"}", "context": {"user_id": "uu"}}
-4:{"event_type": "edx.ui.lms.outline.selected", "event": "{\"target_url\": \"https://pages.local/2/\", \"target_name\": \"m2\"}"}
-5:{"event_type": "edx.grades.problem.submitted", "referer": "https://pages.local/2/", "time": "2018-01-02T09:30:00.000000", "context": {"user_id": "15"}, "event": {"problem_id": "pp"}}
+4:{"event_type": "edx.ui.lms.outline.selected", "event": "{\"target_url\": \"https://pages.local/2/0/\", \"target_name\": \"m2\"}"}
+5:{"event_type": "edx.grades.problem.submitted", "referer": "https://pages.local/2/0/", "time": "2018-01-02T09:30:00.000000", "context": {"user_id": "15"}, "event": {"problem_id": "pp"}}
 6:{"event_type": "problem_check", "event_source": "server", "event": {"problem_id": "pp", "submission": {"t1": {"question": "", "response_type": "type", "correct": false}}}, "context": {"user_id": "15"}}
-7:{"event_type": "edx.grades.problem.submitted", "referer": "https://pages.local/2/#qq", "time": "2018-01-02T10:00:00.000000", "context": {"user_id": "15"}, "event": {"problem_id": "pp"}}
+7:{"event_type": "edx.grades.problem.submitted", "referer": "https://pages.local/2/0/#qq", "time": "2018-01-02T10:00:00.000000", "context": {"user_id": "15"}, "event": {"problem_id": "pp"}}
 8:{"event_type": "problem_check", "event_source": "server", "event": {"problem_id": "pp", "submission": {"t1": {"question": "QQ", "response_type": "type", "correct": true}}}, "context": {"user_id": "15"}}
-9:{"event_type": "openassessmentblock.create_submission", "context": {"user_id": "uu", "module": {"usage_key": "block-v1:a+b+type@openassessment+block@bb", "display_name": "aa"}}, "event": {"submission_uuid": "ps1"}, "referer": "https://pages.local/2/"}
+9:{"event_type": "openassessmentblock.create_submission", "context": {"user_id": "uu", "module": {"usage_key": "block-v1:a+b+type@openassessment+block@bb", "display_name": "aa"}}, "event": {"submission_uuid": "ps1"}, "referer": "https://pages.local/2/0/"}
 10:{"event_type": "openassessmentblock.peer_assess", "context": {"user_id": "u2"}, "event": {"submission_uuid": "ps1", "parts": [{"option": {"points": 2}, "criterion": {"points_possible": 3}}, {"option": {"points": 1}, "criterion": {"points_possible": 2}}]}}""".split('\n')
 
     def test_parse(self):
@@ -70,15 +70,15 @@ class LogsTest(unittest.TestCase):
              ('15', 't1', 1, '02.01.2018 10:00:00')})
         self.assertSetEqual(
             set(report.get_tasks()),
-            {('t1', 'type', 'QQ', '2', 2, 'm2'),
+            {('t1', 'type', 'QQ', '2', 2, 'NA'),
              ('bb', 'openassessment', 'aa',
-              '2', 2, 'm2')})
+              '2', 2, 'NA')})
         self.assertSetEqual(
             set(report.get_student_content()),
             {('uu', 'v1', 1)})
         self.assertSetEqual(
             set(report.get_content()),
-            {('v1', 'video', 'NA', '1', 1, 'm1')})
+            {('v1', 'video', 'NA', '1', 1, 'NA')})
         self.assertSetEqual(
             set(report.get_assessments()),
             {('uu', 'bb', 'u2', 3, 5)})
